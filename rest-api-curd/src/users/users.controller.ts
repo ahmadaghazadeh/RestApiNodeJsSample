1b101061a64d0edbb939ea6211ac4d61e1384fce
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2024-04-04 08:38:59 +0330

    Add UserRepository

--- rest-api-curd/src/users/users.controller.ts
+++ rest-api-curd/src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Post,Body,Param,Delete,Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(
+        private readonly usersService: UsersService
+    ){}
+
+    @Get()
+    async findAll(): Promise<User[]>{
+        return await this.usersService.findAll();
+    }
+
+    @Get(":id")
+    async findOne(id: number): Promise<User>{
+        const user= this.usersService.findOne(id);
+        if(!user) throw new Error("User not found"); //TODO: Throw Error in a right way
+        return user;
+    }
+
+    @Post()
+    async create(@Body() user:User) : Promise<void>{
+        await this.usersService.create(user);
+    }
+
+    @Put(":id")
+    async update(id: number, @Body() user:User) : Promise<void>{
+        const targetUser= this.usersService.findOne(id);
+        if(!targetUser) throw new Error("User not found"); //TODO: Throw Error in a right way
+        this.usersService.update(id,user);
+    }
+
+    @Delete(":id")
+    async delete(id: number) : Promise<void>{
+        const user= this.usersService.findOne(id);
+        if(!user) throw new Error("User not found"); //TODO: Throw Error in a right way
+        this.usersService.delete(id);
+    }
+}
--- rest-api-curd/src/users/users.service.ts
+++ rest-api-curd/src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm'
+import {Repository} from 'typeorm'
+import {User} from './user.entity'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>
+    ){
+    }
+
+    async findAll(): Promise<User[]>{
+        return await  this.usersRepository.find();
+    }
+
+    async findOne(id: number): Promise<User> {
+        return await this.usersRepository.findOne({
+            where : {id}
+        });
+    }
+
+    async create(user: User) : Promise<void>{
+        this.usersRepository.create(user);
+    }
+
+    async update(id: number, user: User) : Promise<void>{
+        this.usersRepository.update(id,user);
+    }
+    
+    async delete(id:number) : Promise<void>{
+        await this.usersRepository.delete(id);
+    }
+}
